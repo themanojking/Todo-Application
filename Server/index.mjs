29ec6ec2c89e612fe3b69376cfa14ccf4d24b53a
themanojking <kmanoj71737@gmail.com>
2025-11-02 12:05:01 +0530

    changingin the index file in the server

--- Server/index.mjs
+++ Server/index.mjs
@@ -17,7 +17,10 @@ app.use(routers);
 const PORT = 5000;
 
 
-mongoose.connect(process.env.MONGODB_URL)
+mongoose.connect(process.env.MONGODB_URI ,{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
 .then(()=> console.log("DB Connected"))
 .catch((err)=> console.log(`Error: ${err}`));
 
